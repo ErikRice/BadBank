21cb73083bcd962cbf71293035a294fc58e4a887
ErikRice <erik.m.rice12@gmail.com>
2021-11-30 21:57:15 -0500

    y

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/account.js";
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 dotenv.config({ path: '.env'})
