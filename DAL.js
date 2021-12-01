ef8f73297ee2452d721e8af841f2e6680a259a82
ErikRice <erik.m.rice12@gmail.com>
2021-11-30 23:52:51 -0500

    y

--- DAL.js
+++ DAL.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config({ path: '.env'})
 let db;
 
-MongoClient.connect(process.env.MONGOLAB_URI, {useUnifiedTopology: true}, (err, client) => {
+MongoClient.connect(process.env.REACT_APP_MONGODB_URI || process.env.MONGOLAB_URI, {useUnifiedTopology: true}, (err, client) => {
     if (err) {
         console.log(`${err}: Couldn't connect to the database`);
     }
