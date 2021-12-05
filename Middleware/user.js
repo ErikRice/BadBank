fef5b9e47f244b33c4c25d02ab0296623969cfc5
ErikRice <erik.m.rice12@gmail.com>
2021-12-04 21:08:05 -0500

    troublshoot deposit

--- Middleware/user.js
+++ Middleware/user.js
@@ -10,7 +10,7 @@ const userAuth = async (req, res, next) => {
         let decodedData;
         if (token) {
                 decodedData = await jwt.verify(token, process.env.REACT_APP_USER_TOKEN_SECRET);
-                if (error) { return res.sendStatus(403).json({message: "Unauthorized"}) };
+                // if (error) { return res.sendStatus(403).json({message: "Unauthorized"}) };
                 req.user = { "id": decodedData.id, "transaction": transaction.transaction };
                 next();
         }
